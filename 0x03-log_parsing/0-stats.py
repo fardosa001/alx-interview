a6c7bd7510950b62394e8c8f300deb41c9af1256
fardosa001 <fardosaadow@gmail.com>
2024-03-28 08:40:17 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,6 +13,7 @@ def print_stats(file_size, status_codes):
 
 line_num = 0
 file_size = 0
+status_code = 0
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
 
@@ -21,12 +22,16 @@ try:
         line_num += 1
         split_line = line.split()
 
-        if len(split_line) >= 7:
+        if len(split_line) > 1:
             file_size += int(split_line[-1])
 
+        if len(split_line) > 2 and split_line[-2].isnumeric():
             status_code = split_line[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+        else:
+            status_code = 0
+
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
 
         if line_num % 10 == 0:
             print_stats(file_size, status_codes)
