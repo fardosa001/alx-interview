38271b9a267674009b894d80a022b7a05e72f36e
fardosa001 <fardosaadow@gmail.com>
2024-04-25 12:57:32 +0000

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,9 +3,6 @@
 const request = require('request');
 const movieId = process.argv[2];
 const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 request(movieUrl, async function (error, response, body) {
   if (error) {
     return console.error(error);
