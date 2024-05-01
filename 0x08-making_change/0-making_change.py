fde914283f29f8b5fa0e004e2e06a10459c15d06
fardosa001 <fardosaadow@gmail.com>
2024-05-01 15:28:39 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,15 +5,16 @@
 def makeChange(coins, total):
     """fewest number of coins needed to
     meet a given amount total"""
+
     coins_used = 0
-    current =  0
+    current = 0
     if total <= 0:
         return 0
 
     coins = reversed(sorted(coins))
 
     for coin in coins:
-       while current + coin <= total:
+        while current + coin <= total:
             coins_used += 1
             current += coin
 
