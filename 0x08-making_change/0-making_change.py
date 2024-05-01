611768cfdff76ae56d3eb470361fa39754349844
fardosa001 <fardosaadow@gmail.com>
2024-05-01 15:25:53 +0000

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """fewest number of coins needed to
+    meet a given amount total"""
+    coins_used = 0
+    current =  0
+    if total <= 0:
+        return 0
+
+    coins = reversed(sorted(coins))
+
+    for coin in coins:
+       while current + coin <= total:
+            coins_used += 1
+            current += coin
+
+    if current != total:
+        return -1
+
+    return coins_used
